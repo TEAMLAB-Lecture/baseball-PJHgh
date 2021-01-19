fba3cc982faec061d80375bc9afdda1ec80e00b9
PJHgh <sset2323@gamil.com>
2021-01-19 16:59:23 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -137,7 +137,7 @@ def get_not_duplicated_three_digit_number():
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
     # get_random_number() 함수를 사용하여 random number 생성
     number = get_random_number()
-    while is_duplicated_number(number):
+    while is_duplicated_number(str(number)):
         number = get_random_number()
     return number
 
